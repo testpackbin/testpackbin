c91e3994bcb4f23764f1f96d0feafda97ce75fbb
asBrettisay <brett.caudill.is@gmail.com>
2016-04-13 14:42:21 -0600

    Added mongoose.
    - Mongoose uses a config file, which will switch DBs based on NODE_ENV.
    - start.js will boot up the server in development mode.

--- server/index.js
+++ server/index.js
@@ -1,7 +1,18 @@
 var express = require('express');
 var user = require('./controllers/userCtrl');
+var mongoose = require('mongoose');
+var config = require('../config');
 
 var app = express();
+
+
+mongoose.connect(config.mongoURI[process.env.NODE_ENV || 'development'], function(err, res) {
+  if (err) {
+    console.log('Error connecting to the database:', err);
+  } else {
+    console.log('Connected to database: ', config.mongoURI[app.settings.env] || 'development');
+  }
+})
 
 app.post('/api/login', user.login);
 
