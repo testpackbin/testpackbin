d970bb3c8171b115133146e31c656e1077459212
asBrettisay <brett.caudill.is@gmail.com>
2016-04-13 13:19:10 -0600

    Basic server which will load up react components. npm start to begin.

--- server/index.js
+++ server/index.js
@@ -6,11 +6,4 @@ app.get('/', function(req, res) {
   return res.send(fs.readFileSync(path.resolve('..', 'index.html')));
 });
 
-var colors = ['red', 'blue', 'green', 'yellow', 'purple'];
-app.get('/api/randomcolor', function (req, res) {
-  res.send({
-    color: colors[Math.floor(Math.random() * (colors.length - 1))]
-  });
-});
-
 module.exports = app;
