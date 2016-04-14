a6e6d5eb83dc23868a5d5148e2fac3628f3d0ce0
Kirill Prostyakov <kprostyakov@gmail.com>
2016-04-14 13:13:30 -0600

    copy jwt from url query

--- app/modules/courses/signals/opened.js
+++ app/modules/courses/signals/opened.js
@@ -3,10 +3,12 @@ import set from 'cerebral-addons/set';
 import copy from 'cerebral-addons/copy';
 import loadCourses from '../actions/loadCourses';
 import showSnackbar from 'common/factories/actions/showSnackbar';
+import setSession from '../actions/setSession';
 
 export default [
   setPage('courses'),
   set('state:/courses.isLoading', true),
+  setSession,
   loadCourses, {
     success: [copy('input:/courses', 'state:/courses.courses')],
     error: [showSnackbar('Error retrieving the course!')]
