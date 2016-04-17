2a782e4df8a063f1eaa1bea832781494f7a99b1f
asBrettisay <brett.caudill.is@gmail.com>
2016-04-17 01:23:25 -0600

    added two more bins to seed file.

--- seed.js
+++ seed.js
@@ -17,7 +17,7 @@ Bin.remove({})
   Bin.insertMany([
     {
       "id" : "reactTest",
-      "author" : "14604998946975184",
+      "author" : "admin",
       "tests" : [
           {
               "name" : "spec.js",
@@ -52,14 +52,82 @@ Bin.remove({})
       ],
       "isLive" : null,
       "readme": "React Test",
-      "Name": "Test out testpackbin with this boilerplate.",
+      "name": "Test out testpackbin with this boilerplate.",
       "subject": "React"
+  },
+  {
+    "id": "angular1",
+    "author": "admin",
+    "tests": [{
+      "name": "spec.js",
+      "content": "describe(\"mainCtrl\",function(){var e,o,t,n;beforeEach(module(\"myApp\")),beforeEach(inject(function(c,i){e=c,o=e.$new(),t=i,n=t(\"mainCtrl\",{$rootScope:e,$scope:o})})),it(\"should exist\",function(){expect(n).toBeDefined()}),it(\"should say 'Hello World'\",function(){var e={};t(\"mainCtrl\",{$scope:e});expect(e.message).toEqual(\"Hello World\")})});"
+    }],
+    "packages" : {
+      "angular" : "1.5.0",
+    },
+    "loaders": {
+      "babel": {
+        "stage0" : true,
+        "es2015" : true
+      }
+    },
+    "files" : [
+      {
+        "name": "index.html",
+        "content": "<!DOCTYPEhtml><html><head><metacharset=\"utf-8\"><title>HelloWorld</title></head><body></div></body></html>"
+      },
+      {
+        "name": "app.js",
+        "content": ""
+      },
+      {
+        "name": "mainCtrl.js",
+        "content": ""
+      }
+    ],
+    "isLive": null,
+    "readme": "## Angular Friends\r\n\r\n### Introduction\r\n\r\nWelcome to Day 1 of AngularJS. Angular is a powerful JavaScript framework created by Google, designed to optimize front-end web development.\r\n\r\n##### Objective\r\n\r\nStudents will learn and utilize Angular directives to create a simple SPA application. They will create an angular module, create a controller, and grab data from said controller to put on the view.\r\n\r\n#####(As a bonus, students can create and use a Service to pull in data from http://jsonplaceholder.typicode.com/)#####\r\n\r\n### !!!!!!!!!!Install Angular!!!!!!!!\r\nWithout an AngularJS script, you will not be able to access any of Angular's capabilities.\r\n\r\n<script src=\"https://ajax.googleapis.com/ajax/libs/angularjs/1.4.5/angular.min.js\"></script>\r\n####\r\n\r\n\r\n####\r\n\r\n* Declare your Angular app. Name it \"myApp\".\r\n\r\n* Create a controller for your app in a separate file. Make sure that the app and the controller are both attached. Name the controller \"mainCtrl\"\r\n\r\nInject $scope into \"mainCtrl\".\r\n\r\n* Add the necessary ng-directives to the index.html file to include your Angular files in the view.\r\n\r\n* Test your controller by adding a scope variable called 'message' with the value 'Hello World' as a string.\r\n\r\n* Bind the $scope variable you just created to the view (index.html) using double curly brackets around the variable name, {{message}}, to see if your controller is working.\r\n\r\n(Hint: Binding the $scope variable to the view will only work if the correct Angular directive put the controller on the view.)\r\n'",
+    "subject": "Angular",
+    "name": "Angular Friends"
+  },
+  {
+    "id": "angular1",
+    "author": "admin",
+    "tests": [{
+      "name": "spec.js",
+      "content": "describe(\"mainCtrl\",function(){var e,o,t,n;beforeEach(module(\"myApp\")),beforeEach(inject(function(c,i){e=c,o=e.$new(),t=i,n=t(\"mainCtrl\",{$rootScope:e,$scope:o})})),it(\"should exist\",function(){expect(n).toBeDefined()}),it(\"should say 'Hello World'\",function(){var e={};t(\"mainCtrl\",{$scope:e});expect(e.message).toEqual(\"Hello World\")})});"
+    }],
+    "packages" : {
+      "angular" : "1.5.0",
+    },
+    "loaders": {
+      "babel": {
+        "stage0" : true,
+        "es2015" : true
+      }
+    },
+    "files" : [
+      {
+        "name": "index.html",
+        "content": "<!DOCTYPEhtml><html><head><metacharset=\"utf-8\"><title>HelloWorld</title></head><body></div></body></html>"
+      },
+      {
+        "name": "app.js",
+        "content": "angular.module('myApp', [])"
+      },
+      {
+        "name": "mainCtrl.js",
+        "content": ""
+      }
+    ],
+    "isLive": null,
+    "readme": "## Angular Friends\r\n\r\n### Introduction\r\n\r\nWelcome to Day 1 of AngularJS. Angular is a powerful JavaScript framework created by Google, designed to optimize front-end web development.\r\n\r\n##### Objective\r\n\r\nStudents will learn and utilize Angular directives to create a simple SPA application. They will create an angular module, create a controller, and grab data from said controller to put on the view.\r\n\r\n#####(As a bonus, students can create and use a Service to pull in data from http://jsonplaceholder.typicode.com/)#####\r\n\r\n### !!!!!!!!!!Install Angular!!!!!!!!\r\nWithout an AngularJS script, you will not be able to access any of Angular's capabilities.\r\n\r\n<script src=\"https://ajax.googleapis.com/ajax/libs/angularjs/1.4.5/angular.min.js\"></script>\r\n####\r\n\r\n\r\n####\r\n\r\n* Declare your Angular app. Name it \"myApp\".\r\n\r\n* Create a controller for your app in a separate file. Make sure that the app and the controller are both attached. Name the controller \"mainCtrl\"\r\n\r\nInject $scope into \"mainCtrl\".\r\n\r\n* Add the necessary ng-directives to the index.html file to include your Angular files in the view.\r\n\r\n* Test your controller by adding a scope variable called 'message' with the value 'Hello World' as a string.\r\n\r\n* Bind the $scope variable you just created to the view (index.html) using double curly brackets around the variable name, {{message}}, to see if your controller is working.\r\n\r\n(Hint: Binding the $scope variable to the view will only work if the correct Angular directive put the controller on the view.)\r\n'",
+    "subject": "Angular",
+    "name": "Angular Friends"
   }
 
 ], (err, docs) => {
     if (err) throw err;
-    console.log('Done!');
-    console.log('Added to database', docs);
     process.exit(0)
   })
 })
