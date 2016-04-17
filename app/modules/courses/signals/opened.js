dd7a42394a0ce8692bd12654ea7b240d90985197
Kirill Prostyakov <kprostyakov@gmail.com>
2016-04-17 01:12:34 -0600

    wip list of links

--- app/modules/courses/signals/opened.js
+++ app/modules/courses/signals/opened.js
@@ -2,24 +2,20 @@ import setPage from 'common/factories/actions/setPage';
 import set from 'cerebral-addons/set';
 import copy from 'cerebral-addons/copy';
 import when from 'cerebral-addons/when';
-import loadCourses from '../actions/loadCourses';
+import loadUser from '../actions/loadUser';
 import showSnackbar from 'common/factories/actions/showSnackbar';
 import setSession from '../actions/setSession';
-
-let userLoaded = when('state:user.user', {
-  loaded:
-  empty:
-});
-
 
 export default [
   setPage('courses'),
   set('state:/courses.isLoading', true),
-  setSession,
-  when('state:/user.user')
-  loadCourses, {
-    success: [copy('input:/courses', 'state:/courses.courses')],
-    error: [showSnackbar('Error retrieving the course!')]
+  when('input:/jwt'), {
+    true: [
+      loadUser, {
+      success: [setSession],
+      error: [showSnackbar('Error retrieving the course!')]
+    }],
+    false: [ ]
   },
   set('state:/courses.isLoading', false)
 ];
