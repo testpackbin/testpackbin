3a81c3b15c9af94e9febe1737333fa96b39b8559
Zach Parduhn <zacharyparduhn@gmail.com>
2016-04-20 13:57:52 -0600

    Add Bin wireframe done

--- app/components/BinForm/index.jsx
+++ app/components/BinForm/index.jsx
@@ -1,5 +1,9 @@
 import React from 'react';
 import {Decorator as Cerebral} from 'cerebral-view-react';
+import style from './styles.css';
+import Add from 'react-icons/lib/md/add-box';
+import AngleLeft from 'react-icons/lib/fa/angle-left';
+import AngleRight from 'react-icons/lib/fa/angle-right';
 
 class BinForm extends React.Component {
   constructor(props) {
@@ -8,8 +12,14 @@ class BinForm extends React.Component {
   }
   render() {
     return (
-      <div>
-        <span style={{color: "white"}}>BinForm</span>
+      <div className={style.wrapper}>
+        <form className={style.formWrapper}>
+          <AngleLeft className={style.arrow}/>NEW BIN /<AngleRight className={style.arrow}/>
+          <input type="text" placeholder="Bin Name..."/>
+          <input type="text" placeholder="Description..."/>
+          <input type="text" placeholder="Overview..."/>
+          <button type="submit" className={style.submitButton}><Add className={style.addIcon}/>Add Bin</button>
+        </form>
       </div>
     );
   }
