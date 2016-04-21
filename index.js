7b5ae9dff18e56b7e8dc306e2fc05519cf072f27
Andrew Kemp <andrewkemp@Andrews-MacBook-Pro.local>
2016-04-21 13:25:33 -0600

    some things

---
+++ index.js
@@ -0,0 +1,19 @@
+var randomArray = function(size){
+  var array = [];
+  for(var i = 0; i < size; i++){
+    array.push(parseInt(Math.random() * 100));
+  }
+  return array;
+}
+
+console.log('test:', randomArray(5));
+
+function bubbleSort(array){
+  var swapped = true;
+
+}
+
+var index = 0;
+for(var number = 0; number <= 100; number ++){
+  var quantity = buckets[number];
+}
